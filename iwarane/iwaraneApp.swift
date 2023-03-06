c8ad3090c993e5816ea7e2281a5ce05918d4e428
Awaleh m Hassan <awalehmhassan@mbp-de-awaleh.home>
2023-03-06 13:06:59 +0100

    initial commit

---
+++ iwarane/iwaraneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iwaraneApp.swift
+//  iwarane
+//
+//  Created by Awaleh m Hassan on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct iwaraneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
